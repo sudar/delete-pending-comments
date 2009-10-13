26dc351b5308a5e5085099beabaf821d3a07897f
Nicolas Kuttler <git@kuttler.eu>
2009-10-13 09:14:45 +0000

    release 0.0.3
    git-svn-id: http://plugins.svn.wordpress.org/delete-pending-comments/tags/0.0.3@162927 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- delete-pending-comments.php
+++ delete-pending-comments.php
@@ -5,7 +5,7 @@ Plugin URI: http://www.nicolaskuttler.de/wordpress/delete-pending-comments/
 Author: Nicolas Kuttler
 Author URI: http://www.nicolaskuttler.de/
 Description: 
-Version: 0.0.1
+Version: 0.0.3
 */
 
 add_action( 'init', 'nkdeletepending_init' );
@@ -16,15 +16,18 @@ function nkdeletepending_init() {
 
 add_action('admin_menu', 'nkdeletepending_add_pages');
 function nkdeletepending_add_pages() {
-	add_options_page(__('Delete Pending', 'delete-pending-comments' ), __('Delete Pending', 'delete-pending-comments' ), 10, 'nkdeletepending', 'nkdeletepending_options_page');
+	add_options_page(__('Delete Pending Comments', 'delete-pending-comments' ), __('Delete Pending Comments', 'delete-pending-comments' ), 10, 'nkdeletepending', 'nkdeletepending_options_page');
 	function nkdeletepending_options_page() { ?>
 		<div class="wrap" style="margin: 5mm; max-width: 80ex;">
 		<?php
 			if ($_POST['nkdeletepending']) {
-				echo '<div id="message" class="updated fade">Form submitted.<br />';
+				echo '<div id="message" class="updated fade">';
 				if ($_POST['nkdeletepending']) {
 					if ( stripslashes($_POST['nkdeletepending']) == __('I am sure I want to delete all pending comments and realize this can\'t be undone', 'delete-pending-comments' ) ) {
 						_e('I deleted all pending comments!', 'delete-pending-comments' );
+					}
+					else {
+						_e('Please try again. Did you copy the text properly?', 'delete-pending-comments' );
 					}
 					$comments = get_comments('status=hold');
 					foreach ($comments as $comment) {
@@ -35,37 +38,45 @@ function nkdeletepending_add_pages() {
 			echo '</div>';
 			}
 		?>
-		<h2>Delete Pending Comments</h2>
+		<h2><?php _e('Delete Pending Comments', 'delete-pending-comments') ?></h2>
 		<p>
-			<?php _e('You have to type the text following text into the form to delete all pending comments', 'delete-pending-comments' ); ?>:
+			<?php _e('You have to type the following text into the form to delete all pending comments', 'delete-pending-comments' ); ?>
 		</p>
 		<blockquote style="font-style: italic;" >
-			<?php _e('I am sure I want to delete all pending comments and realize this can\'t be undone', 'delete-pending-comments' ); ?>.
+			<?php _e('I am sure I want to delete all pending comments and realize this can\'t be undone', 'delete-pending-comments' ); ?>
 		</blockquote>
 
 		<form action="" method="post">
 			<input name="nkdeletepending" type="text" size="80" >
-			<input type="submit" class="button-primary" value="Delete comments">
+			<input type="submit" class="button-primary" value="<?php _e('Delete Pending Comments', 'delete-pending-comments') ?>">
 		</form>
 
-<h3>My other plugins</h3>
-<p>     
-<a href="http://www.nkuttler.de/nktagcloud/">Better tag cloud</a>:
+<h3>My plugins</h3>
+<p>
+<a href="http://www.nkuttler.de/wordpress/nktagcloud/">Better tag cloud</a>:
 I was pretty unhappy with the default WordPress tag cloud widget. This one is more powerful and offers a list HTML markup that is consistent with most other widgets.
 <br/>
-<a href="http://www.nkuttler.de/nkmovecomment/">Move WordPress comments</a>:
-This plugin adds a small form to every comment on your blog. The form is only added for admins and allows you to move comments to a different post/page and to fix comment threading. 
+<a href="http://www.nkuttler.de/wordpress/nkthemeswitch/">Theme switch</a>:
+I like to tweak my main theme that I use on a variety of blogs. If you have ever done this you know how annoying it can be to break things for visitors of your blog. This plugin allows you to use a different theme than the one used for your visitors when you are logged in.
 <br/>
-<a href="http://www.nkuttler.de/nksnow/">Snow and more</a>:
-This one lets you see snowflakes (or other, custom images) fall down your blog.
+<a href="http://www.nkuttler.de/wordpress/zero-conf-mail/">Zero Conf Mail</a>:
+Simple mail contact form, the way I like it. No ajax, no bloat. No configuration necessary, but possible.
 <br/>
-<a href="http://www.nkuttler.de/nkfireworks/">Fireworks</a>:
-The name says it all, see snowflakes on your blog!
+<a href="http://www.nkuttler.de/wordpress/nkmovecomments/">Move WordPress comments</a>:
+This plugin adds a small form to every comment on your blog. The form is only added for admins and allows you to <a href="http://www.nkuttler.de/nkmovecomments/">move comments</a> to a different post/page and to fix comment threading.
+<br/>
+<a href="http://www.nkuttler.de/wordpress/delete-pending-comments">Delete Pending Comments</a>:
+This is a plugin that lets you delete all pending comments at once. Useful for spam victims.
+<br/>
+<a href="http://www.nkuttler.de/wordpress/nksnow/">Snow and more</a>:
+This one lets you see snowflakes, leaves, raindrops, balloons or custom images fall down or float upwards on your blog.
+<br/>
+<a href="http://www.nkuttler.de/wordpress/nkfireworks/">Fireworks</a>:
+The name says it all, see fireworks on your blog!
 <br/>
 <a href="http://www.rhymebox.de/blog/rhymebox-widget/">Rhyming widget</a>:
-I wrote a little online rhyming dictionary. This is a widget to search it directly from one of your sidebars.
-<br/>
-</p> 
+I wrote a little online <a href="http://www.rhymebox.com/">rhyming dictionary</a>. This is a widget to search it directly from one of your sidebars.
+</p>
 		</div>
 		<?php
 	}
---
+++ readme.txt
@@ -0,0 +1,60 @@
+=== Delete Pending Comments ===
+Contributors: nkuttler
+Author URI: http://www.nicolaskuttler.de/
+Plugin URI: http://www.nkuttler.de/wordpress/nkdeletepending/
+Donate link: http://www.amazon.de/gp/registry/24F64AHKD51LY
+Tags: admin, plugin, comments, spam, pending, delete, delete comments, mass delete, mass delete comments
+Requires at least: 2.7
+Tested up to: 2.8.4
+Stable tag: 0.0.3
+
+A quick way to delete all pending comments. Useful for victims of spammer attacks. 
+
+== Description ==
+<p>
+This plugin is a quick way to delete all pending comments. It's useful for victims of spammer attacks. 
+</p>
+
+<h3>My plugins</h3>
+<p>
+<a href="http://www.nkuttler.de/wordpress/nktagcloud/">Better tag cloud</a>:
+I was pretty unhappy with the default WordPress tag cloud widget. This one is more powerful and offers a list HTML markup that is consistent with most other widgets.
+<br/>
+<a href="http://www.nkuttler.de/wordpress/nkthemeswitch/">Theme switch</a>:
+I like to tweak my main theme that I use on a variety of blogs. If you have ever done this you know how annoying it can be to break things for visitors of your blog. This plugin allows you to use a different theme than the one used for your visitors when you are logged in.
+<br/>
+<a href="http://www.nkuttler.de/wordpress/zero-conf-mail/">Zero Conf Mail</a>:
+Simple mail contact form, the way I like it. No ajax, no bloat. No configuration necessary, but possible.
+<br/>
+<a href="http://www.nkuttler.de/wordpress/nkmovecomments/">Move WordPress comments</a>:
+This plugin adds a small form to every comment on your blog. The form is only added for admins and allows you to <a href="http://www.nkuttler.de/nkmovecomments/">move comments</a> to a different post/page and to fix comment threading.
+<br/>
+<a href="http://www.nkuttler.de/wordpress/delete-pending-comments">Delete Pending Comments</a>:
+This is a plugin that lets you delete all pending comments at once. Useful for spam victims.
+<br/>
+<a href="http://www.nkuttler.de/wordpress/nksnow/">Snow and more</a>:
+This one lets you see snowflakes, leaves, raindrops, balloons or custom images fall down or float upwards on your blog.
+<br/>
+<a href="http://www.nkuttler.de/wordpress/nkfireworks/">Fireworks</a>:
+The name says it all, see fireworks on your blog!
+<br/>
+<a href="http://www.rhymebox.de/blog/rhymebox-widget/">Rhyming widget</a>:
+I wrote a little online <a href="http://www.rhymebox.com/">rhyming dictionary</a>. This is a widget to search it directly from one of your sidebars.
+</p>
+
+== Installation ==
+Unzip, upload to your plugin directory, enable the plugin and configure it as needed. If you want to add the widget do that in your dashboard's Design section.
+
+== Screenshots ==
+1. The configuration menu.
+
+== Frequently Asked Questions ==
+None yet.
+
+== Changelog ==
+= 0.0.3 =
+ * More small fixes
+= 0.0.2 =
+ * Small fixes
+= 0.0.1 =
+ * Initial release
